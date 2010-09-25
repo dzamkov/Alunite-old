f05575008880fd3fbfaa664fe1e083415d9bba99
DmitryZamkov <dmitry94@gmail.com>
2010-09-25 08:35:33 -0400

    Restarting in c#, less confusing

---
+++ Alunite/Window.cs
@@ -0,0 +1,40 @@
+﻿using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Graphics.OpenGL;
+
+namespace Alunite
+{
+    /// <summary>
+    /// Main program window
+    /// </summary>
+    public class Window : GameWindow
+    {
+        public Window()
+            : base(640, 480, GraphicsMode.Default, "Alunite")
+        {
+
+
+        }
+
+        /// <summary>
+        /// Program main entry point.
+        /// </summary>
+        public static void Main(string[] Args)
+        {
+            Window w = new Window();
+            w.Run();
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs e)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+
+            this.SwapBuffers();
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            
+        }
+    }
+}
