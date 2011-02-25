260ac536692be4624ca714119645a2de7dc6f9cb
dzamkov <dzamkov@xiatek.org>
2011-02-25 18:53:57 -0500

    Some more unimplemented interfaces

---
+++ Alunite/Particle.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Alunite
+{
+    /// <summary>
+    /// Contains the material properties for a particle excluding position, orientation, velocity and mass, which
+    /// are common to all particles.
+    /// </summary>
+    public interface ISubstance
+    {
+        /// <summary>
+        /// Updates a particle of this substance by the specified amount of time in seconds in the given environment (which is orientated to the particle). Assume
+        /// that the units used in the Environment are consistent with those used for particles (meters, seconds, kilograms). This function should account for every
+        /// force at every scale, including gravity and electromagnetism.
+        /// </summary>
+        void Update(Matter Environment, double Time, out Vector DPosition, out Vector DVelocity, out Quaternion DOrientation, ref double Mass);
+    }
+
+    /// <summary>
+    /// Describes a particle in a certain frame of reference.
+    /// </summary>
+    public struct Particle
+    {
+        public Particle(Vector Position, Vector Velocity, Quaternion Orientation, double Mass, ISubstance Substance)
+        {
+            this.Position = Position;
+            this.Velocity = Velocity;
+            this.Orientation = Orientation;
+            this.Mass = Mass;
+            this.Substance = Substance;
+        }
+
+        /// <summary>
+        /// The gravitational constant which satisfies F = G * (M1 + M2) / (R * R) with F being the force of gravity in newtons, M1 and M2 the mass of the objects
+        /// in kilograms and R the distance between them in meters.
+        /// </summary>
+        public static double G = 6.67428e-11;
+
+        /// <summary>
+        /// The relative location of the particle in meters.
+        /// </summary>
+        public Vector Position;
+
+        /// <summary>
+        /// The relative velocity of the particle in meters per second.
+        /// </summary>
+        public Vector Velocity;
+
+        /// <summary>
+        /// The relative orientation of the particle. This can be ignored for particles
+        /// whose orientation doesn't matter.
+        /// </summary>
+        public Quaternion Orientation;
+
+        /// <summary>
+        /// The mass of the particle in kilograms.
+        /// </summary>
+        public double Mass;
+
+        /// <summary>
+        /// Gets the substance of the particle, which describes the particles properties.
+        /// </summary>
+        public ISubstance Substance;
+    }
+}
--- Alunite/Visualizer.cs
+++ Alunite/Visualizer.cs
@@ -16,6 +16,17 @@ namespace Alunite
         public Visualizer(Matter Matter)
         {
             this._Matter = Matter;
+            this._Particles = new List<_Particle>();
+
+            Random r = new Random();
+            for (int t = 0; t < 100; t++)
+            {
+                this._Particles.Add(new _Particle()
+                {
+                    Position = new Vector(r.NextDouble(), r.NextDouble(), r.NextDouble()),
+                    Velocity = new Vector(0.0, 0.0, 0.0)
+                });
+            }
         }
 
         /// <summary>
@@ -33,7 +44,7 @@ namespace Alunite
         {
             Vector3d up = new Vector3d(0.0, 0.0, 1.0);
             Matrix4d proj = Matrix4d.CreatePerspectiveFieldOfView(Math.Sin(Math.PI / 8.0), this.Size.AspectRatio, 0.1, 100.0);
-            Matrix4d view = Matrix4d.LookAt(new Vector(10.0, 10.0, 10.0), new Vector(0.0, 0.0, 0.0), up);
+            Matrix4d view = Matrix4d.LookAt(new Vector(3.0, 3.0, 3.0), new Vector(0.0, 0.0, 0.0), up);
             GL.MultMatrix(ref proj);
             GL.MultMatrix(ref view);
         }
@@ -44,12 +55,65 @@ namespace Alunite
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
             GL.PointSize(5.0f);
-            GL.Begin(BeginMode.Points);
             GL.Color4(0.0, 0.5, 1.0, 1.0);
-            GL.Vertex3(0.0, 0.0, 0.0);
+            GL.Begin(BeginMode.Points);
+            foreach (_Particle pt in this._Particles)
+            {
+                GL.Vertex3(pt.Position);
+            }
             GL.End();
         }
 
+        public override void Update(GUIControlContext Context, double Time)
+        {
+            List<_Particle> nparts = new List<_Particle>(this._Particles.Count);
+            for(int t = 0; t < this._Particles.Count; t++)
+            {
+                _Particle pt = this._Particles[t];
+                Vector pos = pt.Position;
+                Vector vel = pt.Velocity;
+
+                // Make force vector
+                Vector force = new Vector(0.0, 0.0, -1.0);
+                for(int i = 0; i < this._Particles.Count; i++)
+                {
+                    _Particle opt = this._Particles[i];
+                    if (t != i)
+                    {
+                        Vector away = pos - opt.Position;
+                        double len = Math.Max(away.Length, 0.001);
+                        Vector awayforce = away * (0.001 / (len * len * len));
+                        force += awayforce;
+                    }
+                }
+
+                vel += force * Time;
+                pos += vel * Time;
+
+                double rest = 0.2;
+                if (pos.X > 1.0) { pos.X = 1.0; vel.X = -Math.Abs(vel.X) * rest; }
+                if (pos.Y > 1.0) { pos.Y = 1.0; vel.Y = -Math.Abs(vel.Y) * rest; }
+                if (pos.Z > 1.0) { pos.Z = 1.0; vel.Z = -Math.Abs(vel.Z) * rest; }
+                if (pos.X < 0.0) { pos.X = 0.0; vel.X = Math.Abs(vel.X) * rest; }
+                if (pos.Y < 0.0) { pos.Y = 0.0; vel.Y = Math.Abs(vel.Y) * rest; }
+                if (pos.Z < 0.0) { pos.Z = 0.0; vel.Z = Math.Abs(vel.Z) * rest; }
+
+                nparts.Add(new _Particle()
+                {
+                    Position = pos,
+                    Velocity = vel
+                });
+            }
+            this._Particles = nparts;
+        }
+
+        private struct _Particle
+        {
+            public Vector Velocity;
+            public Vector Position;
+        }
+
+        private List<_Particle> _Particles;
         private Matter _Matter;
     }
 }
