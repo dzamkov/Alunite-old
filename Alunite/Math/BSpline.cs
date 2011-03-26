f1327f632f570a0de5d77df79da9db4bfe4d30d8
dzamkov <dzamkov@xiatek.org>
2011-03-25 21:14:10 -0400

    b-splines

--- Alunite/Math/BSpline.cs
+++ Alunite/Math/BSpline.cs
@@ -8,6 +8,53 @@ namespace Alunite
     /// </summary>
     public static class BSpline
     {
+        /// <summary>
+        /// Evaluates a bspline defines by an a knot vector and a control point vector of equal lengths at a certain parameter.
+        /// </summary>
+        public static T Evaluate<T, TInterpolation>(TInterpolation Interpolation, double[] Knots, T[] Points, int Degree, double Parameter)
+            where TInterpolation : IInterpolation<T>
+        {
+            int l = GetInterval(Knots, Parameter) + Degree;
 
+            T[] temp = new T[Degree + 1];
+            for (int i = 0; i < temp.Length; i++)
+            {
+                temp[i] = Points[l - i];
+            }
+            for (int k = 0; k < Degree; k++)
+            {
+                for (int m = 0; m < Degree - k; m++)
+                {
+                    int u = l - m;
+                    double ui = u < Degree ? Knots[0] : Knots[u - Degree];
+                    double a = (Parameter - ui) / (Knots[u - k] - ui);
+                    temp[m] = Interpolation.Mix(temp[m + 1], temp[m], a);
+                }
+            }
+
+            return temp[0];
+        }
+
+        /// <summary>
+        /// Gets the highest interval (index of a knot) whose value is before the given parameter.
+        /// </summary>
+        public static int GetInterval(double[] Knots, double Parameter)
+        {
+            int l = 0;
+            int h = Knots.Length;
+            while (h > l + 1)
+            {
+                int s = (l + h) / 2;
+                if (Parameter >= Knots[s])
+                {
+                    l = s;
+                }
+                else
+                {
+                    h = s;
+                }
+            }
+            return l;
+        }
     }
 }
--- Alunite/Program.cs
+++ Alunite/Program.cs
@@ -17,6 +17,10 @@ namespace Alunite
         /// </summary>
         public static void Main(string[] Args)
         {
+            double[] knots = new double[] { 0.0, 0.2, 0.4, 0.6, 0.8, 1.0 };
+            double[] points = new double[] { 0.0, 1.0, 0.0, 1.0, 0.0, 1.0 };
+            double res = BSpline.Evaluate(ScalarInterpolation.Singleton, knots, points, 2, 0.6);
+
             CameraEntity camsensor = Entity.Camera();
             Entity cambody = Entity.Brush(Substance.Iron, Shape.Sphere(0.1)).Apply(new Transform(-0.12, 0.0, 0.0));
             Entity cam = camsensor.Embody(cambody);
